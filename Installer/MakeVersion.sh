df65283f45f263efe08bbea65c4ff18f347a59aa
Peter Pflaeging <peter@pflaeging.net>
2009-08-11 16:03:37 +0000

    Updated Java libraries. Step up to version 1.7. This is a release!

---
+++ Installer/MakeVersion.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+VERSION=1.7
+SVNVERSION=`/usr/local/bin/svnversion . |  cut -d: -f2`
+SRCPATH=dist/ship/
+echo "Making Releases for:" $VERSION.$SVNVERSION
+cd $SRCPATH
+mv PortableSigner-Generic.zip PortableSigner-Generic-$VERSION.$SVNVERSION.zip
+mv PortableSigner-Install.jar PortableSigner-Install-$VERSION.$SVNVERSION.jar
+mv PortableSigner-Installer.exe PortableSigner-Installer-$VERSION.$SVNVERSION.exe
+mv PortableSigner-MacOSX.tar.gz PortableSigner-MacOSX-$VERSION.$SVNVERSION.tar.gz
+mv PortableSigner-Windows.zip PortableSigner-Windows-$VERSION.$SVNVERSION.zip
+rm PortableSigner-MacOSX.tar
